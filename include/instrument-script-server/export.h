dfed82c8568b620caaed0ff720cea8ac322990f5
tjk144 <tkovach@wisc.edu>
2026-06-19 11:16:24 -0500

    Fixed project exports to not define plugin exports

--- include/instrument-script-server/export.h
+++ include/instrument-script-server/export.h
@@ -7,14 +7,8 @@
 #else
 #define INSTRUMENT_SERVER_API __declspec(dllimport)
 #endif
-#ifdef INSTRUMENT_PLUGIN_EXPORTS
-#define INSTRUMENT_PLUGIN_API __declspec(dllexport)
-#else
-#define INSTRUMENT_PLUGIN_API
-#endif
 #else
 #define INSTRUMENT_SERVER_API
-#define INSTRUMENT_PLUGIN_API
 #endif
 
 #endif // INSTRUMENT_SERVER_EXPORT_H
