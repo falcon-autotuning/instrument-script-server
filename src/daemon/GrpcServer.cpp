b3bac607ac570e221f4c07065c6b919598a29365
Daniel Schug <daschug1@gmail.com>
2026-07-01 14:11:58 -0400

    feat: error routed

--- src/daemon/GrpcServer.cpp
+++ src/daemon/GrpcServer.cpp
@@ -6,131 +6,137 @@
 
 namespace instserver::daemon {
 
+namespace {
+grpc::Status to_grpc_status(int rc, const v1::StandardResponse &resp, const std::string &default_fail_msg) {
+  if (rc == 0) {
+    return grpc::Status::OK;
+  }
+  if (!resp.ok() && resp.has_error()) {
+    const auto &err = resp.error();
+    std::string msg = err.message().empty() ? default_fail_msg : err.message();
+    switch (err.code()) {
+      case v1::ERROR_CODE_INVALID_ARGUMENT:
+        return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, msg);
+      case v1::ERROR_CODE_FILE_DOES_NOT_EXIST:
+      case v1::ERROR_CODE_BUFFER_NOT_FOUND:
+        return grpc::Status(grpc::StatusCode::NOT_FOUND, msg);
+      default:
+        return grpc::Status(grpc::StatusCode::INTERNAL, msg);
+    }
+  }
+  return grpc::Status(grpc::StatusCode::INTERNAL, default_fail_msg);
+}
+
+template <typename TResponse>
+grpc::Status to_grpc_status(int rc, const TResponse &resp, const std::string &default_fail_msg) {
+  if (rc == 0) {
+    return grpc::Status::OK;
+  }
+  if (resp.has_standard_response()) {
+    return to_grpc_status(rc, resp.standard_response(), default_fail_msg);
+  }
+  return grpc::Status(grpc::StatusCode::INTERNAL, default_fail_msg);
+}
+}
+
 class DaemonServiceImpl final : public v1::DaemonService::Service {
 public:
   grpc::Status DaemonStatus(grpc::ServerContext *context,
                             const v1::DaemonStatusRequest *req,
                             v1::DaemonStatusResponse *resp) override {
     int rc = handle_daemon_status(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "DaemonStatus failed");
+    return to_grpc_status(rc, *resp, "DaemonStatus failed");
   }
 
   grpc::Status StartInstrument(grpc::ServerContext *context,
                                const v1::StartInstrumentRequest *req,
                                v1::StartInstrumentResponse *resp) override {
     int rc = handle_start_instrument(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "StartInstrument failed");
+    return to_grpc_status(rc, *resp, "StartInstrument failed");
   }
 
   grpc::Status StopInstrument(grpc::ServerContext *context,
                               const v1::StopInstrumentRequest *req,
                               v1::StopInstrumentResponse *resp) override {
     int rc = handle_stop_instrument(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "StopInstrument failed");
+    return to_grpc_status(rc, *resp, "StopInstrument failed");
   }
 
   grpc::Status InstrumentStatus(grpc::ServerContext *context,
                                 const v1::InstrumentStatusRequest *req,
                                 v1::InstrumentStatusResponse *resp) override {
     int rc = handle_instrument_status(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "InstrumentStatus failed");
+    return to_grpc_status(rc, *resp, "InstrumentStatus failed");
   }
 
   grpc::Status ListInstruments(grpc::ServerContext *context,
                                const v1::ListInstrumentsRequest *req,
                                v1::ListInstrumentsResponse *resp) override {
     int rc = handle_list_instruments(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "ListInstruments failed");
+    return to_grpc_status(rc, *resp, "ListInstruments failed");
   }
 
   grpc::Status MeasureJob(grpc::ServerContext *context,
                           const v1::MeasureJobRequest *req,
                           v1::MeasureJobResponse *resp) override {
     int rc = handle_measure_job(*req, resp);
-    return rc == 0
-               ? grpc::Status::OK
-               : grpc::Status(grpc::StatusCode::INTERNAL, "MeasureJob failed");
+    return to_grpc_status(rc, *resp, "MeasureJob failed");
   }
 
   grpc::Status JobStatus(grpc::ServerContext *context,
                          const v1::JobStatusRequest *req,
                          v1::JobStatusResponse *resp) override {
     int rc = handle_job_status(*req, resp);
-    return rc == 0
-               ? grpc::Status::OK
-               : grpc::Status(grpc::StatusCode::INTERNAL, "JobStatus failed");
+    return to_grpc_status(rc, *resp, "JobStatus failed");
   }
 
   grpc::Status MeasureJobResult(grpc::ServerContext *context,
                                 const v1::MeasureJobResultRequest *req,
                                 v1::MeasureJobResultResponse *resp) override {
     int rc = handle_measure_job_result(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "MeasureJobResult failed");
+    return to_grpc_status(rc, *resp, "MeasureJobResult failed");
   }
 
   grpc::Status JobList(grpc::ServerContext *context,
                        const v1::JobListRequest *req,
                        v1::JobListResponse *resp) override {
     int rc = handle_job_list(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL, "JobList failed");
+    return to_grpc_status(rc, *resp, "JobList failed");
   }
 
   grpc::Status CancelJob(grpc::ServerContext *context,
                          const v1::CancelJobRequest *req,
                          v1::CancelJobResponse *resp) override {
     int rc = handle_cancel_job(*req, resp);
-    return rc == 0
-               ? grpc::Status::OK
-               : grpc::Status(grpc::StatusCode::INTERNAL, "CancelJob failed");
+    return to_grpc_status(rc, *resp, "CancelJob failed");
   }
 
   grpc::Status Discover(grpc::ServerContext *context,
                         const v1::DiscoverRequest *req,
                         v1::DiscoverResponse *resp) override {
     int rc = handle_discover(*req, resp);
-    return rc == 0
-               ? grpc::Status::OK
-               : grpc::Status(grpc::StatusCode::INTERNAL, "Discover failed");
+    return to_grpc_status(rc, *resp, "Discover failed");
   }
 
   grpc::Status ListDataBuffers(grpc::ServerContext *context,
                                const v1::ListDataBuffersRequest *req,
                                v1::ListDataBuffersResponse *resp) override {
     int rc = handle_list_buffers(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "ListDataBuffers failed");
+    return to_grpc_status(rc, *resp, "ListDataBuffers failed");
   }
 
   grpc::Status ReleaseBuffer(grpc::ServerContext *context,
                              const v1::ReleaseBufferRequest *req,
                              v1::ReleaseBufferResponse *resp) override {
     int rc = handle_release_buffer(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "ReleaseBuffer failed");
+    return to_grpc_status(rc, *resp, "ReleaseBuffer failed");
   }
 
   grpc::Status GetBufferMetadata(grpc::ServerContext *context,
                                  const v1::GetBufferMetadataRequest *req,
                                  v1::GetBufferMetadataResponse *resp) override {
     int rc = handle_get_buffer_metadata(*req, resp);
-    return rc == 0 ? grpc::Status::OK
-                   : grpc::Status(grpc::StatusCode::INTERNAL,
-                                  "GetBufferMetadata failed");
+    return to_grpc_status(rc, *resp, "GetBufferMetadata failed");
   }
 
   grpc::Status StopDaemon(grpc::ServerContext *context,
@@ -140,9 +146,7 @@ public:
     if (resp) {
       resp->set_ok(rc == 0);
     }
-    return rc == 0
-               ? grpc::Status::OK
-               : grpc::Status(grpc::StatusCode::INTERNAL, "StopDaemon failed");
+    return to_grpc_status(rc, *resp, "StopDaemon failed");
   }
 };
 
